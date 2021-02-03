df0317f5c22fcc7d38f6a374b597dfc35acf3073
jjaffeux <j.jaffeux@gmail.com>
2021-02-03 12:23:28 +0100

    DEV: uses more descriptive cookie name

--- javascripts/discourse/initializers/init-discourse-disable-jump-reply.js
+++ javascripts/discourse/initializers/init-discourse-disable-jump-reply.js
@@ -1,7 +1,7 @@
 import cookie, { removeCookie } from "discourse/lib/cookie";
 import { withPluginApi } from "discourse/lib/plugin-api";
 
-export const COOKIE_NAME = "ddjr";
+export const COOKIE_NAME = "discourse-disable-jump-to-reply";
 
 export default {
   name: "init-discourse-disable-jump-reply",
