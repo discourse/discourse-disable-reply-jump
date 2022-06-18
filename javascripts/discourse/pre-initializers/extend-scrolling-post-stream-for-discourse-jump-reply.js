137bc812bd00a7cd19301265191fdf5d9489766b
Jarek Radosz <jradosz@gmail.com>
2022-06-18 21:22:07 +0200

    DEV: Add CI setup (#7)

--- javascripts/discourse/pre-initializers/extend-scrolling-post-stream-for-discourse-jump-reply.js
+++ javascripts/discourse/pre-initializers/extend-scrolling-post-stream-for-discourse-jump-reply.js
@@ -3,12 +3,11 @@ import { SETTING_NAME } from "../initializers/init-discourse-disable-jump-reply"
 
 export default {
   name: "extend-scrolling-post-stream-for-discourse-jump-reply",
-
   before: "inject-discourse-objects",
 
   initialize() {
     ScrollingPostStream.reopen({
-      _posted(staged) {
+      _posted() {
         if (localStorage.getItem(SETTING_NAME) === "true") {
           return;
         }
