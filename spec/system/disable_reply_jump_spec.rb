353ce96368af7a2a5ef701895c29d8c26034f26b
Jarek Radosz <jradosz@gmail.com>
2024-06-04 13:21:41 +0200

    DEV: Minor cleanup (#21)
    1. modifyClass instead of reopen
    2. native input instead of Input component
    3. use Topic#url
    4. whitespace
    
    also fixes a flaky spec

--- spec/system/disable_reply_jump_spec.rb
+++ spec/system/disable_reply_jump_spec.rb
@@ -7,16 +7,12 @@ RSpec.describe "Disable Reply Jump", system: true do
   fab!(:post_1) {
     Fabricate(:post, raw: "long\ncontent\nto\nforce\nscrolling\non\npage\nlong\ncontent\nto\nforce\nscrolling\non\npage\nlong\ncontent\nto\nforce\nscrolling\non\npage\nlong\ncontent\nto\nforce\nscrolling\non\npage\nlong\ncontent\nto\nforce\nscrolling\non\npage\nlong\ncontent\nto\nforce\nscrolling\non\npage\n", topic: topic_1)
   }
-  fab!(:post_2) {
-    Fabricate(:post, topic: topic_1)
-  }
-  fab!(:post_3) {
-    Fabricate(:post, topic: topic_1)
-  }
+  fab!(:post_2) { Fabricate(:post, topic: topic_1) }
+  fab!(:post_3) { Fabricate(:post, topic: topic_1) }
 
   fab!(:user) { Fabricate(:user, trust_level: TrustLevel[1], refresh_auto_groups: true) }
 
-  before do 
+  before do
     sign_in(user)
   end
 
@@ -36,11 +32,11 @@ RSpec.describe "Disable Reply Jump", system: true do
   end
 
   it "scroll position does not change after replying when localStorage is set" do
-    visit("/t/#{topic_1.id}")
+    visit(topic_1.url)
 
     page.execute_script("localStorage.setItem('discourse-disable-jump-to-reply', 'true');")
 
-    find("#post_1 .reply").click
+    find(".timeline-footer-controls .reply-to-post").click
     find(".d-editor-textarea-wrapper textarea").set("This is my test reply content")
     find(".save-or-cancel .create").click
 
@@ -48,8 +44,7 @@ RSpec.describe "Disable Reply Jump", system: true do
     expect(page).not_to have_css(".saving-text")
 
     scroll_position = page.evaluate_script("window.scrollY;")
-  
+
     expect(scroll_position).to eq(0)
   end
- 
 end
